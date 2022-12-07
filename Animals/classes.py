aa9e48c39b8f7abb006a9d01252361a059e62a54
Raymond Hale <r.hale@gmail.com>
2022-12-07 18:09:47 +0100

    Added Cat and Dog classes

--- Animals/classes.py
+++ Animals/classes.py
@@ -6,3 +6,13 @@ class Animal(ABC):
     @abstractmethod
     def talk(self):
         pass
+
+
+class Dog(Animal):
+
+    def talk(self):
+        print("I say 'WOOF'.")
+
+
+class Cat(Animal):
+    pass
